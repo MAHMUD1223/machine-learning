b4de81a4a47a8bbf5f1e76cc3c24cdd41b1f57ca
Mahmud <mardhaka67@gmail.com>
2024-02-26 09:42:27 +0600

    made model and saving it

---
+++ learning/yt_auto_bookrec/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+## edit below variables as per your requirements -
+REPO_NAME = "machine-learning"
+AUTHOR_USER_NAME = "mahmud1223"
+SRC_REPO = "src"
+LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for Movie Recommender System",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    author_email="mardhaka67@gmail.com",
+    packages=[SRC_REPO],
+    license="BSD 3-Clause License",
+    python_requires=">=3.7",
+    install_requires=LIST_OF_REQUIREMENTS
+)
